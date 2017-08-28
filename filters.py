11998a82d77ea2dc1b3b351d2dd2eb2e2283a0b3
Kunal Patel <kunalpatelx92@gmail.com>
2017-08-28 15:36:45 -0500

    Apply Brightness & Contrast to Image

--- filters.py
+++ filters.py
@@ -8,6 +8,12 @@ cv2.namedWindow('app')
 def dummy(val):
   pass
 
+# Store Image in the color_original variable
+color_original = cv2.imread('TomBrady.jpg')
+
+# Make an Image copy to store modified image into
+color_modified = color_original.copy()
+
 # Create Track bars within window to represent filters
 cv2.createTrackbar('contrast', 'app', 1, 100, dummy)
 cv2.createTrackbar('brightness', 'app', 50, 100, dummy)
@@ -16,9 +22,18 @@ cv2.createTrackbar('grayscale', 'app', 0, 1, dummy)
 
 # Loop to keep window open until 'q' key is pressed
 while True:
+  # Display the Modified Image to the window
+  cv2.imshow('app', color_modified)
   k = cv2.waitKey(1) & 0xFF
   if (k == ord('q')):
     break
+
+  # Store Track bar positions in variables
+  contrast = cv2.getTrackbarPos('contrast', 'app')
+  brightness = cv2.getTrackbarPos('brightness', 'app')
+
+  # Add Color Modifications to the Original Image
+  color_modified = cv2.addWeighted(color_original, contrast, np.zeros(color_original.shape, dtype=color_original.dtype), 0, brightness - 50)
 
 # Destroy Window
 cv2.destroyAllWindows()
